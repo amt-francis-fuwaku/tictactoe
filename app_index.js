b21c20e4d0b6c00fc5cfbc88a268382286171833
francisfuwaku <francisfuwaku@amalitech.org>
2023-05-17 11:06:37 -0700

    update: html(index)

--- app_index.js
+++ app_index.js
@@ -1,9 +1,11 @@
 // clear before storage
 sessionStorage.clear();
 const xBackgroundColor = document.getElementById("x-img-id");
+console.log("x background color", xBackgroundColor);
 const oBackgroundColor = document.getElementById("o-img-id");
-const xSelect = document.getElementById("x");
-const oSelect = document.getElementById("o");
+const xIcon = document.getElementById("x");
+console.log("x background color", xIcon);
+const oIcon = document.getElementById("o");
 
 const OSVG =
     '  <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" fill="#F2B137"/></svg>';
@@ -38,10 +40,10 @@ sessionStorage.setItem("opponent", JSON.stringify(X_MARK));
 // toggles
 const selectX = () => {
     xBackgroundColor.style.backgroundColor = "#A8BFC9";
-    xSelect.style.filter =
+    xIcon.style.filter =
         "invert(12%) sepia(12%) saturate(1823%) hue-rotate(157deg) brightness(96%) contrast(90%)";
     oBackgroundColor.style.backgroundColor = "#1A2A33";
-    oSelect.style.filter =
+    oIcon.style.filter =
         "invert(79%) sepia(32%) saturate(145%) hue-rotate(153deg) brightness(90%) contrast(89%)";
     sessionStorage.setItem("user", JSON.stringify(X_MARK));
     sessionStorage.setItem("opponent", JSON.stringify(O_MARK));
@@ -49,10 +51,10 @@ const selectX = () => {
 
 const selectO = () => {
     oBackgroundColor.style.backgroundColor = "#A8BFC9";
-    oSelect.style.filter =
+    oIcon.style.filter =
         "invert(12%) sepia(12%) saturate(1823%) hue-rotate(157deg) brightness(96%) contrast(90%)";
     xBackgroundColor.style.backgroundColor = "#1A2A33";
-    xSelect.style.filter =
+    xIcon.style.filter =
         "invert(79%) sepia(32%) saturate(145%) hue-rotate(153deg) brightness(90%) contrast(89%)";
 
     sessionStorage.setItem("user", JSON.stringify(O_MARK));
